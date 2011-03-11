d8540505ec31069c1219858923592608cbc33fc3
Chuck D'Antonio <c_dantonio@acm.org>
2011-03-10 23:50:14 -0500

    Updated the PHP module to enable APC with a default configuration when using php-fpm.

--- puppet/modules/php/manifests/init.pp
+++ puppet/modules/php/manifests/init.pp
@@ -5,4 +5,12 @@ class php {
     ensure => present,
   }
   
+  file { "/etc/php5/php-fpm/apc.ini":
+    require => Package["php5-fpm"],
+    owner => root,
+    group => root,
+    mode => 644,
+    source => "puppet:///php/apc.ini"
+  }
+  
 }
