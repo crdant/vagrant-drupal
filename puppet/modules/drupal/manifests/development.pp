fd5e9a65143edf85f2c5e384a9ab64657faac0b0
Chuck D'Antonio <c_dantonio@acm.org>
2011-03-12 12:10:09 -0500

    Removed development drupal class since it wasn't doing much different that I'd do for the site in different environments.  Development specific items are in the box specific manifest right now.

--- puppet/modules/drupal/manifests/development.pp
+++
@@ -1,6 +0,0 @@
-# Developers using vagrant will have their project directory at /vagrant on the virtual 
-# machine.  In this situation, we'll take advantage of the filesystem layout to load up
-# their development instance
-class drupal::development {
-  
-}
